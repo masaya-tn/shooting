e85fd3469622506f06e3f0c60a8f8a4d1633a4d5
masaya-tn <uvermasayan@icloud.com>
2022-10-13 23:20:30 +0900

    弾の発射まで

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+//デバッグのフラグ
+const DEBUG = true;
+
+let drawCount=0;
+let fps=0;
+let lastTime=Date.now();
+
+// スムージング
+const SMOOTHING = false;
 
 //ゲームスピード(ms)
 const GAME_SPEED = 1000/60;
@@ -22,6 +31,10 @@ let can = document.getElementById("can");
 let con = can.getContext("2d");
 can.width  = CANVAS_W;
 can.height = CANVAS_H;
+con.mozimageSmoothingEnagbled   = SMOOTHING;
+con.webkitimageSmoothingEnabled = SMOOTHING;
+con.msimageSmoothingEnabled     = SMOOTHING;
+con.imageSmoothingEnabled       = SMOOTHING;
 
 //フィールド（仮想画面）
 let vcan = document.createElement("canvas");
@@ -36,74 +49,112 @@ let camera_y = 0;
 //星の実体
 let star=[];
 
-//整数のランダムを作る
-function rand(min,max)
-{
-	return Math.floor( Math.random()*(max-min+1) )+min;
-}
+//キーボードの状態
+let key=[];
 
-//星クラス
-class Star
-{
-	constructor()
-	{
-		this.x  = rand(0,FIELD_W)<<8;
-		this.y  = rand(0,FIELD_H)<<8;
-		this.vx = 0;
-		this.vy = rand(30,200);
-		this.sz = rand(1,2);
-	}
-	
-	draw()
-	{
-		let x=this.x>>8;
-		let y=this.y>>8;
-		
-		if( x<camera_x || x>=camera_x+SCREEN_W 
-			|| y<camera_y || y>=camera_y+SCREEN_H )return;
-		
-		vcon.fillStyle=rand(0,2)!=0?"66f":"#8af";
-		vcon.fillRect(x,y,this.sz,this.sz);
-		
-	}
-	
-	update()
-	{
-		this.x += this.vx;
-		this.y += this.vy;
-		if( this.y>FIELD_H<<8 )
-		{
-			this.y=0;
-			this.x=rand(0,FIELD_W)<<8;
-		}
-	}
-	
-}
+//オブジェクト達
+let teki=[];
+let tama=[];
+let jiki = new Jiki();
+//teki[0]= new Teki( 75, 200<<8,200<<8, 0,0);
+
+//ファイルを読み込み
+let spriteImage = new Image();
+spriteImage.src = "sprite.png";
+
 
 //ゲーム初期化
 function gameInit()
 {
 	for(let i=0;i<STAR_MAX;i++)star[i]= new Star();
 	setInterval( gameLoop , GAME_SPEED );
+	// 本気でやりたい時は requestAnimationFrameを使いましょう
 }
-//ゲームループ
-function gameLoop()
+
+//オブジェクトをアップデート
+function updateObj( obj )
 {
-	//移動の処理
-	
-	for(let i=0;i<STAR_MAX;i++)star[i].update();
-	
+	for(let i=obj.length-1;i>=0;i--)
+	{
+		obj[i].update();
+		if(obj[i].kill)obj.splice( i,1 );
+	}
+}
+
+//オブジェクトを描画
+function drawObj( obj )
+{
+	for(let i=0;i<obj.length;i++)obj[i].draw();
+}
+
+
+//移動の処理
+function updateAll()
+{
+	updateObj(star);
+	updateObj(tama);
+	updateObj(teki);
+	jiki.update();
+}
+
+//描画の処理
+function drawAll()
+{
 	//描画の処理
 	
 	vcon.fillStyle="black";
-	vcon.fillRect(0,0,SCREEN_W,SCREEN_H);
+	vcon.fillRect(camera_x,camera_y,SCREEN_W,SCREEN_H);
 	
-	for(let i=0;i<STAR_MAX;i++)star[i].draw();
+	drawObj( star );
+	drawObj( tama );
+	jiki.draw();
+	drawObj( teki );
+	
+	// 自機の範囲 0 ～ FIELD_W
+	// カメラの範囲 0 ～ (FIELD_W-SCREEN_W)
+	
+	camera_x = (jiki.x>>8)/FIELD_W * (FIELD_W-SCREEN_W);
+	camera_y = (jiki.y>>8)/FIELD_H * (FIELD_H-SCREEN_H);
 	
 	//仮想画面から実際のキャンバスにコピー
 	
 	con.drawImage( vcan ,camera_x,camera_y,SCREEN_W,SCREEN_H,
 		0,0,CANVAS_W,CANVAS_H);
+}
+
+//情報の表示
+function putInfo()
+{
+	if(DEBUG)
+	{
+		drawCount++;
+		if( lastTime +1000 <= Date.now() )
+		{
+			fps=drawCount;
+			drawCount=0;
+			lastTime=Date.now();
+		}
+		
+		
+		con.font="20px 'Impact'";
+		con.fillStyle ="white";
+		con.fillText("FPS :"+fps,20,20);
+		con.fillText("Tama:"+tama.length,20,40);
+		con.fillText("Teki:"+teki.length,20,60);
+	}
+}
+
+//ゲームループ
+function gameLoop()
+{
+	//テスト的に敵を出す
+	
+	if( 1 )//rand(0,10)==1 )
+		teki.push(new Teki(39,rand(0,FIELD_W)<<8,0, 0,rand(300,1200)));
+	
+	updateAll();
+	drawAll();
+	putInfo();
 }
 
 //オンロードでゲーム開始
